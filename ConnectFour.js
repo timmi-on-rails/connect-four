e910f46ba4224c7e7044a4555579c5a77720b66b
AppVeyor <dd-tom@web.de>
2019-03-21 19:17:09 +0000

    deploy script

--- ConnectFour.js
+++ ConnectFour.js
@@ -312,7 +312,7 @@ Bridge.assembly("ConnectFour", function ($asm, globals) {
             },
             ctors: {
                 init: function () {
-                    this.Info = "1.0.4";
+                    this.Info = "1.0.5";
                 }
             }
         }
