1b9b025ee7670c79eaa5b3d40df44849e9fa1212
Tom <dd-tom@web.de>
2019-03-21 19:56:49 +0100

    added Version Info

---
+++ ConnectFour/Version.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Do not add anything to this file.
+ * Content will be replaced during CI builds.
+ */
+
+namespace ConnectFour
+{
+	static class Version
+	{
+		public const string Info = "development";
+	}
+}
