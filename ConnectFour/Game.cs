97b32c90b4a21647768e599b340aa5ed0898c34c
Tom <dd-tom@web.de>
2019-03-21 19:25:04 +0100

    initial source code

---
+++ ConnectFour/Game.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour
+{
+	delegate void GameUpdateHandler(Game game);
+
+	class Game
+	{
+		public event GameUpdateHandler OnUpdate;
+
+		public enum Chip
+		{
+			NONE,
+			C1,
+			C2
+		}
+
+		public Chip currentChip = Chip.C1;
+
+		private const int rows = 6;
+		private const int cols = 7;
+
+		public Chip[,] chips = new Chip[rows, cols];
+
+		public IController Controller1 { get; set; }
+
+		public IController Controller2 { get; set; }
+
+		private int win = 4;
+
+		public async Task Run()
+		{
+			
+			while (true)
+			{
+				List<Move> moves = new List<Move>();
+				for (int i = 0; i < cols; i++)
+					if (chips[0, i] == Chip.NONE)
+						moves.Add(new Move { Index = i });
+
+				Move m = currentChip == Chip.C1 ? await Controller1.Select(moves) : await Controller2.Select(moves);
+
+				if (CheckForWin(m))
+				{
+					Console.WriteLine("player " + currentChip + " wins!");
+					OnUpdate?.Invoke(this);
+					break;
+				}
+
+				OnUpdate?.Invoke(this);
+
+				currentChip = currentChip == Chip.C1 ? Chip.C2 : Chip.C1;
+			}
+		}
+
+		private bool CheckForWin(Move m)
+		{
+			int i;
+			for (i = 0; i < rows; i++)
+				if (chips[i, m.Index] != 0)
+					break;
+			chips[i - 1, m.Index] = currentChip;
+			int row = i - 1;
+			int col = m.Index;
+
+			if (Row(row, col, 0, 1) || Row(row, col, 1, 0) || Row(row, col, 1, 1) || Row(row, col, 1, -1))
+				return true;
+
+			return false;
+		}
+
+		private bool Row(int row, int col, int drow, int dcol)
+		{
+			return C(drow, dcol) + C(-drow, -dcol) + 1 >= win;
+
+			int C(int drow2, int dcol2)
+			{
+				int c = 0;
+				for (int i = 1; i < win; i++)
+					if (col + i * dcol2 >= 0 && col + i * dcol2 < cols && row + i * drow2 >= 0 && row + i * drow2 < rows && chips[row + i * drow2, col + i * dcol2] == currentChip)
+						if (currentChip == chips[row + i * drow2, col + i * dcol2])
+							c++;
+						else break;
+				return c;
+			}
+		}
+	}
+}
--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bridge.Html5;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,109 @@ using System.Threading.Tasks;
 
 namespace ConnectFour
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public delegate void ButtonHandler(int index);
+		public delegate void ColorHandler(string color);
+
+		static HTMLCanvasElement canvas;
+		static event ButtonHandler Selection;
+		static event ColorHandler Color;
+
+
+		public static void Main()
 		{
+			canvas = new HTMLCanvasElement();
+
+			var divElement = new HTMLDivElement();
+
+			Func<int, HTMLButtonElement> createButton = (int index) =>
+			{
+				var button = new HTMLButtonElement
+				{
+					InnerHTML = (index + 1).ToString(),
+					OnClick = (e) => { Selection?.Invoke(index); }
+				};
+				button.Style.Width = "50px";
+				button.Style.Height = "50px";
+				button.Style.MarginRight = "10px";
+				button.Style.MarginBottom = "10px";
+				Color += (c) => button.Style.Background = c;
+
+				return button;
+
+			};
+
+
+			divElement.AppendChild(createButton(0));
+			divElement.AppendChild(createButton(1));
+			divElement.AppendChild(createButton(2));
+			divElement.AppendChild(createButton(3));
+			divElement.AppendChild(createButton(4));
+			divElement.AppendChild(createButton(5));
+			divElement.AppendChild(createButton(6));
+
+			Document.Body.AppendChild(divElement);
+			Document.Body.AppendChild(canvas);
+
+
+
+
+
+			Console.WriteLine("Hello");
+
+
+			Game game = new Game();
+
+			Game_OnUpdate(game);
+			Color?.Invoke(game.currentChip == Game.Chip.C1 ? "#f0f000" : "#00f0f0");
+
+			game.OnUpdate += Game_OnUpdate;
+
+			var controller1 = new SomeController("Player 1");
+			game.Controller1 = controller1;
+
+			SomeController controller2 = new SomeController("Player 2");
+			game.Controller2 = controller2;
+
+			Selection += (e) =>
+			{
+				if (game.currentChip == Game.Chip.C1)
+					controller1.ButtonClick(e);
+				else
+					controller2.ButtonClick(e);
+			};
+
+			game.Run();
+		}
+
+		private static void Game_OnUpdate(Game game)
+		{
+			Color?.Invoke(game.currentChip == Game.Chip.C2 ? "#f0f000" : "#00f0f0");
+
+			var ctx = (CanvasRenderingContext2D)canvas.GetContext("2d");
+
+			canvas.Width = 550;
+			canvas.Height = 500;
+
+			for (int row = 0; row < game.chips.GetLength(0); row++)
+				for (int col = 0; col < game.chips.GetLength(1); col++)
+				{
+					string color = "#f0f0f0";
+
+					if (game.chips[row, col] == Game.Chip.C1)
+					{
+						color = "#f0f000";
+					}
+
+					if (game.chips[row, col] == Game.Chip.C2)
+					{
+						color = "#00f0f0";
+					}
+
+					ctx.FillStyle = color;
+					ctx.FillRect(col * 60, row * 60, 50, 50);
+				}
 		}
 	}
 }
